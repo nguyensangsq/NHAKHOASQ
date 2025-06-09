4f85c7f305b5b06d6e47a029020bc66c8486a5e2
nguyensangsq <nguyensang1992dentist@gmail.com>
2025-06-09 22:01:03 +0700

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,1 +1,13 @@
+// Thay bằng cấu hình thật từ Firebase
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 

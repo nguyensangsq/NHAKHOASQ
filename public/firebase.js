2c4b06fda01957b60aa4f58949f77464a8b5c0d3
nguyensangsq <nguyensang1992dentist@gmail.com>
2025-06-09 22:21:41 +0700

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,24 +1,22 @@
-// Thay bằng cấu hình thật từ Firebase
+// Kết nối Firebase Web SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+// Cấu hình Firebase cho web app của bạn
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyD4kBnKUES4hKcXDMC9hw3w22TlC6z-umk",
+  authDomain: "sq-gibhup.firebaseapp.com",
+  projectId: "sq-gibhup",
+  storageBucket: "sq-gibhup.appspot.com",
+  messagingSenderId: "5000802843931",
+  appId: "1:5000802843931:web:738f3d48091961ab4bf7be"
 };
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+// Khởi tạo Firebase
+const app = initializeApp(firebaseConfig);
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+// Khởi tạo Firestore
+const db = getFirestore(app);
 
+// Xuất `db` để các file khác có thể dùng
+export { db };
